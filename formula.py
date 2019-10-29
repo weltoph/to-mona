a3c656427050d722d82e9cf222adb63856ea3694
Christoph Welzel <welzel@in.tum.de>
2019-10-29 12:32:05 +0100

    further work on the more expressive frontend

--- formula.py
+++ formula.py
@@ -4,23 +4,68 @@ from typing import List, Iterable, Set, Dict, Optional
 class FormulaError(Exception):
     pass
 
-class Term(object):
+class Formula(object):
     def __repr__(self):
-        return f"{type(self)}<{self}>"
+        return f"{type(self)}<{str(self)}>"
 
-@dataclass
-class Constant(Term):
-    value: int
+    def __repr__(self) -> str:
+        return f"{type(self)}<{self}>"
 
     @property
     def variables(self) -> Set["Variable"]:
         return set()
 
     @property
-    def variable(self) -> Optional["Variable"]:
-        return None
+    def sorted_variables(self) -> List["Variable"]:
+        return sorted(self.variables, key=str)
 
-    def rename(self, renaming):
+    def rename(self, renaming : Dict[str, str]) -> "Term":
+        raise NotImplemented
+
+    @property
+    def all_terms(self) -> Set["Term"]:
+        return set()
+
+    @property
+    def sorted_terms(self) -> List["Term"]:
+        return sorted(self.all_terms, key=str)
+
+    @property
+    def variable_terms(self) -> List["Term"]:
+        return [term for term in self.sorted_terms if not term.is_constant]
+
+    @property
+    def complex_terms(self) -> List["Term"]:
+        return [term for term in self.sorted_terms if not term.is_atomic]
+
+    @property
+    def involved_variables(self) -> List["Term"]:
+        return [term for term in self.sorted_terms
+                if term.is_atomic and not term.is_constant]
+
+class Term(Formula):
+    @property
+    def all_terms(self) -> Set["Term"]:
+        return {self} | self.sub_terms
+
+    @property
+    def sub_terms(self) -> Set["Term"]:
+        return set()
+
+    @property
+    def is_constant(self) -> bool:
+        return False
+
+    @property
+    def is_atomic(self) -> bool:
+        return self.is_constant
+
+
+@dataclass
+class Constant(Term):
+    value: str
+
+    def rename(self, renaming) -> "Constant":
         return Constant(self.value)
 
     def __hash__(self):
@@ -33,11 +78,7 @@ class Constant(Term):
         return str(self.value)
 
     @property
-    def terms(self) -> List[Term]:
-        return []
-
-    @property
-    def trivial_term(self) -> bool:
+    def is_constant(self) -> bool:
         return True
 
 @dataclass
@@ -47,10 +88,6 @@ class Variable(Term):
     @property
     def variables(self) -> Set["Variable"]:
         return {self}
-
-    @property
-    def variable(self) -> "Variable":
-        return self
 
     def rename(self, renaming):
         return Variable(renaming.get(self.name, self.name))
@@ -65,19 +102,21 @@ class Variable(Term):
         return self.name
 
     @property
-    def terms(self) -> List[Term]:
-        return [self]
-
-    @property
-    def trivial_term(self) -> bool:
+    def is_atomic(self) -> bool:
         return True
 
 @dataclass
 class Successor(Term):
+    index: int # in {0, 1}
     argument: Term
 
+    def __hash__(self):
+        return hash(self.index) & hash(self.argument)
+
     def __eq__(self, other):
-        return type(self) == type(other) and self.argument == other.argument
+        return type(self) == type(other) and (
+                self.argument == other.argument
+                and self.index == other.index)
 
     @property
     def trivial_term(self) -> bool:
@@ -85,64 +124,120 @@ class Successor(Term):
 
     @property
     def variables(self) -> Set[Variable]:
-        return {self.variable}
+        return self.argument.variables
+
+    def rename(self, renaming : Dict[str, str]) -> "Successor":
+        return Successor(self.index, self.argument.rename(renaming))
 
     @property
-    def variable(self) -> Variable:
-        return self.argument.variable
-
-    def rename(self, renaming):
-        return Successor(self.argument.rename(renaming))
-
-    @property
-    def terms(self) -> List[Term]:
-        return [self] + self.argument.terms
+    def sub_terms(self) -> Set[Term]:
+        return self.argument.all_terms
 
     def __str__(self) -> str:
-        return f"succ({self.argument})"
-
-class Formula(object):
-    def __repr__(self):
-        return f"{type(self)}<{str(self)}>"
+        return f"succ{self.index}({self.argument})"
 
 @dataclass
-class Port(Formula):
+class Predicate(Formula):
     name: str
     argument: Term
 
+    def __hash__(self):
+        return hash(self.name) & hash(self.argument)
+
+    def __eq__(self, other):
+        return type(self) == type(other) and (
+                self.name == other.name and self.argument == other.argument)
+
     @property
     def variables(self) -> Set[Variable]:
-        return {self.variable} if self.variable else set()
+        return self.argument.variables
 
     @property
-    def variable(self) -> Optional[Variable]:
-        return self.argument.variable
+    def all_terms(self) -> Set[Term]:
+        return self.argument.all_terms
 
     def rename(self, renaming):
-        return Port(self.name, self.argument.rename(renaming))
+        return Predicate(self.name, self.argument.rename(renaming))
 
     def __str__(self):
         return f"{self.name}({self.argument})"
 
+@dataclass
+class PredicateCollection(Formula):
+    predicates: Set[Predicate]
+
     @property
-    def terms(self) -> List[Term]:
-        return self.argument.terms
+    def variables(self) -> Set[Variable]:
+        from functools import reduce
+        return set(reduce(
+            lambda acc, p: p.variables | acc,
+            self.predicates,
+            set()))
+
+    @property
+    def all_terms(self) -> Set[Term]:
+        from functools import reduce
+        return set(reduce(
+            lambda acc, p: p.all_terms | acc,
+            self.predicates,
+            set()))
+
+    def rename(self, renaming: Dict[str, str]) -> "PredicateCollection":
+        return type(self)({p.rename(renaming) for p in self.predicates})
+
+    def __str__(self) -> str:
+        return f" {self.comparison_symbol} ".join([str(p) for
+            p in self.predicates])
 
 @dataclass
-class Comparison(Formula):
+class PredicateConjunction(PredicateCollection):
+    @property
+    def comparison_symbol(self) -> str:
+        return "&"
+
+@dataclass
+class PredicateDisjunction(PredicateCollection):
+    @property
+    def comparison_symbol(self) -> str:
+        return "|"
+
+@dataclass
+class Restriction(Formula):
+    pass
+
+@dataclass
+class Last(Restriction):
+    index: int
+    argument: Term
+
+    @property
+    def variables(self) -> Set[Variable]:
+        return self.argument.variables
+
+    @property
+    def all_terms(self) -> Set[Term]:
+        return self.argument.all_terms
+
+    def rename(self, renaming: Dict[str, str]) -> "Last":
+        return Last(self.index, self.argument.rename(renaming))
+
+    def __str__(self):
+        return f"last{self.index}({self.argument})"
+
+@dataclass
+class Comparison(Restriction):
     left: Term
     right: Term
 
     @property
-    def variables(self) -> Iterable[Variable]:
+    def variables(self) -> Set[Variable]:
         return self.left.variables | self.right.variables
 
     @property
-    def terms(self) -> Iterable[Term]:
-        terms = [self.left, self.right]
-        return [t for i, t in enumerate(terms) if not t in terms[:i]]
+    def all_terms(self) -> Set[Term]:
+        return self.left.all_terms | self.right.all_terms
 
-    def rename(self, renaming):
+    def rename(self, renaming: Dict[str, str]) -> "Comparison":
         return type(self)(self.left.rename(renaming),
                 self.right.rename(renaming))
 
@@ -150,25 +245,25 @@ class Comparison(Formula):
         return f"{self.left} {self.comp_str} {self.right}"
 
 @dataclass
-class Smaller(Comparison):
+class Below(Comparison):
     @property
     def comp_str(self):
         return "<"
 
 @dataclass
-class Bigger(Comparison):
-    @property
-    def comp_str(self):
-        return ">"
-
-@dataclass
-class SmallerEqual(Comparison):
+class BelowEqual(Comparison):
     @property
     def comp_str(self):
         return "<="
 
 @dataclass
-class BiggerEqual(Comparison):
+class Above(Comparison):
+    @property
+    def comp_str(self):
+        return ">"
+
+@dataclass
+class AboveEqual(Comparison):
     @property
     def comp_str(self):
         return ">="
@@ -185,187 +280,173 @@ class Unequal(Comparison):
     def comp_str(self):
         return "~="
 
-class TrueGuard(Formula):
-    def rename(self, renaming) -> "Guard":
-        return TrueGuard()
+@dataclass
+class RestrictionCollection(Restriction):
+    restrictions: List[Restriction]
 
     @property
-    def variables(self) -> Iterable[Variable]:
-        return set()
+    def variables(self) -> Set[Variable]:
+        from functools import reduce
+        return set(reduce(
+            lambda acc, r: r.variables | acc,
+            self.restrictions,
+            set()))
 
     @property
-    def terms(self) -> List[Term]:
-        return []
+    def all_terms(self) -> Set[Term]:
+        from functools import reduce
+        return set(reduce(
+            lambda acc, r: r.all_terms | acc,
+            self.restrictions,
+            set()))
 
-    @property
-    def is_trivial(self) -> bool:
-        return True
+    def rename(self, renaming) -> "RestrictionCollection":
+        return type(self)({r.rename(renaming) for r in self.restrictions})
 
-    def __str__(self):
-        return "true"
+    def __str__(self) -> str:
+        restrictions = f" {self.comparison_symbol} ".join(
+                [str(r) for r in self.restrictions])
+        return f"( {restrictions} )"
 
 @dataclass
-class Guard(Formula):
-    comparisons: List[Comparison]
-
+class RestrictionConjunction(RestrictionCollection):
     @property
-    def is_trivial(self) -> bool:
-        return not bool(self.comparisons)
+    def comparison_symbol(self) -> str:
+        return "&"
 
+@dataclass
+class RestrictionDisjunction(RestrictionCollection):
     @property
-    def variables(self) -> Iterable[Variable]:
-        ret = set()
-        for c in self.comparisons:
-            ret |= c.variables
-        return ret
-
-    @property
-    def terms(self) -> List[Term]:
-        ret = []
-        for c in self.comparisons:
-            ret += c.terms
-        return [t for i, t in enumerate(ret) if not t in ret[:i]]
-
-    def rename(self, renaming) -> "Guard":
-        return Guard([c.rename(renaming) for c in self.comparisons])
-
-    def __str__(self):
-        return " & ".join([str(c) for c in self.comparisons])
+    def comparison_symbol(self) -> str:
+        return "|"
 
 @dataclass
 class Broadcast(Formula):
     variable: Variable
-    guard: Guard
-    port: Port
-    index: int
+    guard: RestrictionDisjunction
+    body: PredicateDisjunction
 
-    def rename(self, renaming):
+    def rename(self, renaming: Dict[str, str]) -> "Broadcast":
         variable = self.variable.rename(renaming)
         guard = self.guard.rename(renaming)
-        port = self.port.rename(renaming)
-        return Broadcast(variable, guard, port, self.index)
+        body = self.body.rename(renaming)
+        return Broadcast(variable, guard, body)
 
     def __post_init__(self):
-        if self.variable != self.port.variable:
-            raise FormulaError(f"Broadcast variable mismatch")
+        for predicate in self.body.predicates:
+            if [self.variable] != predicate.involved_variables:
+                raise FormulaError(f"Broadcast variable mismatch")
 
     @property
     def free_variables(self) -> Set[Variable]:
         return self.guard.variables - {self.variable}
 
     @property
-    def terms(self) -> List[Term]:
-        terms = [self.variable]
-        terms += self.guard.terms
-        terms += self.port.terms
-        return [t for i, t in enumerate(terms) if not t in terms[:i]]
+    def all_terms(self) -> Set[Term]:
+        terms = {self.variable}
+        terms |= self.guard.all_terms
+        terms |= self.body.all_terms
+        return terms
 
     @property
     def local_terms(self) -> List[Term]:
-        terms = self.terms
-        return [t for t in terms if t.variable == self.variable]
-
-    @property
-    def contains_not_trivial_local_term(self) -> bool:
-        result = any([not t.trivial_term for t in self.local_terms])
-        return result
+        return [t for t in all_terms if t.variable == self.variable]
 
     def __str__(self):
-        return f"all {self.variable}: {self.guard}. {self.port}"
+        return "broadcasting {{ {variable}: {guard}. {body} }}".format(
+                variable=self.variable, guard=self.guard, body=self.body)
 
 @dataclass
 class Clause(Formula):
-    variables: List[Variable]
-    guard: Guard
-    ports: List[Port]
+    guard: RestrictionConjunction
+    ports: PredicateConjunction
     broadcasts: List[Broadcast]
-    index: int
 
-    def _check_for_unknowns(self) -> None:
-        used_variables = set()
-        for p in self.ports:
-            used_variables |= p.variables
-        for b in self.broadcasts:
-            used_variables |= b.free_variables
-        used_variables |= self.guard.variables
-        unknowns = {g for g in used_variables if not g in self.variables}
-        if unknowns:
-            raise FormulaError(f"{unknowns} are not quantified but used")
+#     def _check_for_unknowns(self) -> None:
+#         used_variables = set()
+#         for p in self.ports:
+#             used_variables |= p.variables
+#         for b in self.broadcasts:
+#             used_variables |= b.free_variables
+#         used_variables |= self.guard.variables
+#         unknowns = {g for g in used_variables if not g in self.variables}
+#         if unknowns:
+#             raise FormulaError(f"{unknowns} are not quantified but used")
 
-    def _check_for_unnecessary_quantification(self) -> None:
-        used_variables = set()
-        for p in self.ports:
-            used_variables |= p.variables
-        unneeded = {g for g in self.variables if g not in used_variables}
-        if unneeded:
-            raise FormulaError(f"{unneeded} are quantified but not used")
+#     def _check_for_unnecessary_quantification(self) -> None:
+#         used_variables = set()
+#         for p in self.ports:
+#             used_variables |= p.variables
+#         unneeded = {g for g in self.variables if g not in used_variables}
+#         if unneeded:
+#             raise FormulaError(f"{unneeded} are quantified but not used")
 
-    def __post_init__(self):
-        self._check_for_unknowns()
-        self._check_for_unnecessary_quantification()
-        unique_var_list = [v for i, v in enumerate(self.variables)
-                if not v in self.variables[:i]]
-        renaming = {v.name: f"x{i}" for i, v in enumerate(unique_var_list)}
-        self.variables = [v.rename(renaming) for v in unique_var_list]
-        self.guard = self.guard.rename(renaming)
-        self.ports = [p.rename(renaming) for p in self.ports]
-        broadcasts = []
-        for i, b in enumerate(self.broadcasts):
-            renaming[b.variable.name] = f"x{len(self.variables) + i}"
-            broadcasts.append(b.rename(renaming))
-        self.broadcasts = broadcasts
+#     def __post_init__(self):
+#         self._check_for_unknowns()
+#         self._check_for_unnecessary_quantification()
+#         unique_var_list = [v for i, v in enumerate(self.variables)
+#                 if not v in self.variables[:i]]
+#         renaming = {v.name: f"x{i}" for i, v in enumerate(unique_var_list)}
+#         self.variables = [v.rename(renaming) for v in unique_var_list]
+#         self.guard = self.guard.rename(renaming)
+#         self.ports = [p.rename(renaming) for p in self.ports]
+#         broadcasts = []
+#         for i, b in enumerate(self.broadcasts):
+#             renaming[b.variable.name] = f"x{len(self.variables) + i}"
+#             broadcasts.append(b.rename(renaming))
+#         self.broadcasts = broadcasts
 
-    @property
-    def terms_in_guard(self) -> List[Term]:
-        ret = list(self.variables)
-        ret += self.guard.terms
-        for p in self.ports:
-            ret += p.terms
-        return [t for i, t in enumerate(ret) if t not in ret[:i]]
+#     @property
+#     def terms_in_guard(self) -> List[Term]:
+#         ret = list(self.variables)
+#         ret += self.guard.terms
+#         for p in self.ports:
+#             ret += p.terms
+#         return [t for i, t in enumerate(ret) if t not in ret[:i]]
 
-    @property
-    def contains_not_trivial_existential_term(self) -> bool:
-        return any([not t.trivial_term for t in self.existential_terms])
+#     @property
+#     def contains_not_trivial_existential_term(self) -> bool:
+#         return any([not t.trivial_term for t in self.existential_terms])
 
-    @property
-    def terms(self) -> List[Term]:
-        ret = self.terms_in_guard
-        for b in self.broadcasts:
-            ret += b.terms
-        return [t for i, t in enumerate(ret) if t not in ret[:i]]
+#     @property
+#     def terms(self) -> List[Term]:
+#         ret = self.terms_in_guard
+#         for b in self.broadcasts:
+#             ret += b.terms
+#         return [t for i, t in enumerate(ret) if t not in ret[:i]]
 
-    @property
-    def existential_terms(self) -> List[Term]:
-        return [t for t in self.terms if t.variable in self.variables]
+#     @property
+#     def existential_terms(self) -> List[Term]:
+#         return [t for t in self.terms if t.variable in self.variables]
 
-    @property
-    def all_ports(self) -> List[Port]:
-        all_ports = self.ports
-        for b in self.broadcasts:
-            all_ports += [b.port]
-        return [p for i, p in enumerate(all_ports) if not p in all_ports[:i]]
+#     @property
+#     def all_ports(self) -> List[Predicate]:
+#         all_ports = self.ports
+#         for b in self.broadcasts:
+#             all_ports += [b.port]
+#         return [p for i, p in enumerate(all_ports) if not p in all_ports[:i]]
 
-    @property
-    def broadcast_terms(self) -> List[Term]:
-        return [t for t in self.terms if t not in self.terms_in_guard]
+#     @property
+#     def broadcast_terms(self) -> List[Term]:
+#         return [t for t in self.terms if t not in self.terms_in_guard]
 
-    def __str__(self):
-        variables = ", ".join([str(v) for v in self.variables])
-        guard = f" {self.guard}."
-        ports = " & ".join([str(p) for p in self.ports])
-        if self.broadcasts:
-            broadcasts = " with " + " & ".join([str(b) for b in self.broadcasts])
-        else:
-            broadcasts = ""
-        return f"{variables}:{guard} {ports}{broadcasts}"
+#     def __str__(self):
+#         variables = ", ".join([str(v) for v in self.variables])
+#         guard = f" {self.guard}."
+#         ports = " & ".join([str(p) for p in self.ports])
+#         if self.broadcasts:
+#             broadcasts = " with " + " & ".join([str(b) for b in self.broadcasts])
+#         else:
+#             broadcasts = ""
+#         return f"{variables}:{guard} {ports}{broadcasts}"
 
-@dataclass
-class Interaction(Formula):
-    clauses: List[Clause]
+# @dataclass
+# class Interaction(Formula):
+#     clauses: List[Clause]
 
-    @property
-    def ports(self) -> List[Port]:
-        ports = []
-        for c in self.clauses:
-            ports += c.ports
-        return [p for i, p in enumerate(ports) if not p in ports[:i]]
+#     @property
+#     def ports(self) -> List[Predicate]:
+#         ports = []
+#         for c in self.clauses:
+#             ports += c.ports
+#         return [p for i, p in enumerate(ports) if not p in ports[:i]]
--- parser.py
+++ parser.py
@@ -1,21 +1,21 @@
-from lark import Lark, Transformer
-
-from system import Component, System
-from formula import *
-from bounded import *
+from lark import Lark, Transformer, v_args
 
 class ParserError(Exception):
     pass
 
 grammar = """
-    system: components interaction            -> system
-          | components interaction properties -> system_with_custom_properties
+    system: components interaction                -> plain_system
+          | components interaction mona_additions -> restricted_system
 
     components: component (component)*
 
-    properties: custom_property (custom_property)*
+    mona_additions: mona_addition (mona_addition)*
+
+    mona_addition: (custom_property|custom_assumption) (custom_property|custom_assumption)*
 
     custom_property: "property" STRING "{" STRING "}"
+
+    custom_assumption: "assumption" STRING "{" STRING "}"
 
     component: "Component" COMPONENTNAME "<" STATENAME ">" "{" transition (transition)* "}"
     transition: STATENAME "->" STATENAME "->" STATENAME
@@ -39,40 +39,36 @@ grammar = """
     broadcast_body: generell_guard "." predicate_disjunction -> guarded_broadcast
                   | predicate_disjunction                    -> unguarded_broadcast
 
-    generell_guard: restriction "&" restriction -> comparison_conjunct
-                  | restriction "|" restriction -> comparison_disjunct
-                  | restriction                 -> comparison_atom
+    generell_guard: conjunctive_guard ("|" conjunctive_guard)*
 
     conjunctive_guard: restriction ("&" restriction)*
 
-    restriction: term comparison_symbol term -> comparison
-               | "last0(" term ")"           -> last_left
-               | "last1(" term ")"           -> last_right
+    restriction: term COMPARISONSYMBOL term -> comparison
+               | "last0(" term ")"          -> last_left
+               | "last1(" term ")"          -> last_right
 
-    comparison_symbol: "~=" -> neq
-                     | "="  -> eq
-                     | "<"  -> lt
-                     | "<=" -> leq
-                     | ">"  -> gt
-                     | ">=" -> geq
+    COMPARISONSYMBOL: "~=" | "=" | "<" | "<=" | ">" | ">="
 
     predicate_conjunction: predicate ("&" predicate)*
     predicate_disjunction: predicate ("|" predicate)*
 
     predicate: NAME "(" term ")"
 
-    term: left_succ
-        | right_succ
-        | variable
-        | constant
+    ?term: left_succ
+         | right_succ
+         | variable
+         | constant
 
     variable: NAME
 
     left_succ:  "succ0(" term ")"
     right_succ: "succ1(" term ")"
-    constant: INT
+    constant: "root"                              -> constant_root
+            | constant "."  CONSTANT_PROLONGATION -> constant_term
 
-    NAME: /(?!(succ0|succ1|last0|last1))[a-zA-Z][a-zA-Z0-9]*/
+    CONSTANT_PROLONGATION: ("0"|"1")
+
+    NAME: /(?!(succ0|succ1|last0|last1|root))[a-zA-Z][a-zA-Z0-9]*/
     COMMENT: "#" /.*/ NEWLINE
 
     %import common.INT
@@ -85,14 +81,134 @@ grammar = """
 
 parser = Lark(grammar, start="system")
 
+class TermParser(Transformer):
+    @v_args(inline=True)
+    def constant_root(self):
+        from formula import Constant
+        return Constant("root")
+
+    @v_args(inline=True)
+    def constant_term(self, current, prolongation):
+        from formula import Constant
+        return Constant(f"{current.value}.{str(prolongation)}")
+
+    @v_args(inline=True)
+    def left_succ(self, term):
+        from formula import Successor
+        return Successor(0, term)
+
+    @v_args(inline=True)
+    def right_succ(self, term):
+        from formula import Successor
+        return Successor(1, term)
+
+    @v_args(inline=True)
+    def variable(self, name):
+        from formula import Variable
+        return Variable(str(name))
+
+class PredicateParser(Transformer):
+    @v_args(inline=True)
+    def predicate(self, name, argument):
+        from formula import Predicate
+        return Predicate(str(name), argument)
+
+    def predicate_conjunction(self, predicates):
+        from formula import PredicateConjunction
+        return PredicateConjunction(set(predicates))
+
+    def predicate_disjunction(self, predicates):
+        from formula import PredicateDisjunction
+        return PredicateDisjunction(set(predicates))
+
+class RestrictionParser(Transformer):
+    @v_args(inline=True)
+    def comparison(self, left, symbol, right):
+        from formula import Below, BelowEqual, Above, AboveEqual
+        from formula import Equal, Unequal
+        lookup = {
+                "~=": Unequal,
+                "=" : Equal,
+                "<" : Below,
+                "<=": BelowEqual,
+                ">" : Above,
+                ">=": AboveEqual,
+                }
+        return lookup[str(symbol)](left, right)
+
+    @v_args(inline=True)
+    def last_left(self, argument):
+        from formula import Last
+        return Last(0, argument)
+
+    @v_args(inline=True)
+    def last_right(self, argument):
+        from formula import Last
+        return Last(1, argument)
+
+class GuardParser(Transformer):
+    def conjunctive_guard(self, restrictions):
+        from formula import RestrictionConjunction
+        return RestrictionConjunction(restrictions)
+
+    def generell_guard(self, conjunctions):
+        from formula import RestrictionDisjunction
+        return RestrictionDisjunction(conjunctions)
+
+class BroadcastParser(Transformer):
+    @v_args(inline=True)
+    def unguarded_broadcast(self, disjunction):
+        from formula import RestrictionConjunction, RestrictionDisjunction
+        return (RestrictionDisjunction([RestrictionConjunction([])]),
+            disjunction)
+
+    @v_args(inline=True)
+    def guarded_broadcast(self, guard, disjunction):
+        return (guard, disjunction)
+
+    @v_args(inline=True)
+    def broadcast(self, variable, body):
+        from formula import Broadcast
+        return Broadcast(variable, *body)
+
+class ClauseParser(Transformer):
+    @v_args(inline=True)
+    def unguarded_clause_body(self, conjunction):
+        from formula import RestrictionConjunction
+        return (RestrictionConjunction([]), conjunction)
+
+    @v_args(inline=True)
+    def guarded_clause_body(self, guard, conjunction):
+        return (guard, conjunction)
+
+    @v_args(inline=True)
+    def local_clause(self, body):
+        from formula import Clause
+        return Clause(*body, [])
+
+    @v_args(inline=True)
+    def mixed_clause(self, body, *broadcasts):
+        from formula import Clause
+        return Clause(*body, broadcasts)
+
+    def broadcasting_clause(self, broadcasts):
+        from formula import Clause, RestrictionConjunction
+        from formula import PredicateConjunction
+        return Clause(RestrictionConjunction([]), PredicateConjunction(set()),
+                broadcasts)
+
 if __name__ == "__main__":
     import sys
     files = [filename for filename in sys.argv[1:] if filename[-3:] == "sys"]
     for filename in files:
         try:
+            print(filename)
             with open(filename, "r") as f:
                 text = f.read()
-                tree = parser.parse(text)
-                print(tree)
+                transformer = (TermParser() * PredicateParser()
+                        * RestrictionParser() * GuardParser()
+                        * BroadcastParser() * ClauseParser())
+                tree = transformer.transform(parser.parse(text))
+                print(tree.pretty())
         except IsADirectoryError:
             continue
